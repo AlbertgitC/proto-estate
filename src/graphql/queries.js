f8028fce1ca676c62022b6923fca67887db1891b
Albert Cheng <lptalbert@gmail.com>
2021-02-02 01:33:48 -0800

    add aws graphql api

---
+++ src/graphql/queries.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getListing = /* GraphQL */ `
+  query GetListing($id: ID!) {
+    getListing(id: $id) {
+      id
+      authorId
+      title
+      content
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listListings = /* GraphQL */ `
+  query ListListings(
+    $filter: ModelListingFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listListings(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        authorId
+        title
+        content
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateListing = /* GraphQL */ `
+  subscription OnCreateListing {
+    onCreateListing {
+      id
+      authorId
+      title
+      content
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateListing = /* GraphQL */ `
+  subscription OnUpdateListing {
+    onUpdateListing {
+      id
+      authorId
+      title
+      content
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteListing = /* GraphQL */ `
+  subscription OnDeleteListing {
+    onDeleteListing {
+      id
+      authorId
+      title
+      content
+      createdAt
+      updatedAt
+    }
+  }
+`;
