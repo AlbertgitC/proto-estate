39355266b533b8ca863e472766977508934af3d6
Albert Cheng <lptalbert@gmail.com>
2021-02-02 02:49:24 -0800

    implement listings query

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+import * as queries from './graphql/queries';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [listings, setListings] = useState([]);
+	
+	useEffect(() => {
+		getListings().then((res) => {
+			setListings(res.data.listListings.items);
+		}).catch((error) => {
+			console.log(error);
+		});
+	}, []);
+
+	async function getListings() {
+		try {
+			return await API.graphql({
+				query: queries.listListings
+			});
+		} catch (err) {
+			return err;
+		};
+	};
+
+	return (
+		<div className="App">
+			<ul>
+				{listings.map((listing, i) =>
+					<li key={i}>
+						<h4>{listing.title}</h4>
+						<p>{listing.content}</p>
+					</li>
+				)}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
