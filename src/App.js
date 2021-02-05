613c9edaad78bf62759593d6d10736bc44459a28
Albert Cheng <lptalbert@gmail.com>
2021-02-05 04:29:20 -0800

    add landing

--- src/App.js
+++ src/App.js
@@ -4,42 +4,45 @@ import * as queries from './graphql/queries';
 import Header from './components/header';
 import { useDispatch } from 'react-redux';
 import * as AuthActions from './util/actions/auth_actions';
+import Landing from './components/landing';
 
 function App() {
-	const [listings, setListings] = useState([]);
+	// const [listings, setListings] = useState([]);
 	const dispatch = useDispatch();
 	
 	useEffect(() => {
-		getListings().then((res) => {
-			setListings(res.data.listListings.items);
-		}).catch((error) => {
-			console.log(error);
-		});
+		// getListings().then((res) => {
+		// 	setListings(res.data.listListings.items);
+		// }).catch((error) => {
+		// 	console.log(error);
+		// });
 
 		Auth.currentAuthenticatedUser()
 			.then(res => {
 				dispatch(AuthActions.signIn(res));
 			})
 			.catch(err => {
-				console.log("error finding user:", err);
+				console.log("user not sign in:", err);
 			});
 	}, []);
 
-	async function getListings() {
-		try {
-			return await API.graphql({
-				query: queries.listListings,
-				authMode: "AWS_IAM"
-			});
-		} catch (err) {
-			return err;
-		};
-	};
+	// async function getListings() {
+	// 	try {
+	// 		return await API.graphql({
+	// 			query: queries.listListings,
+	// 			authMode: "AWS_IAM"
+	// 		});
+	// 	} catch (err) {
+	// 		return err;
+	// 	};
+	// };
 
 	return (
 		<div className="App">
 			<Header />
-			<div></div>
+			<div>
+				<Landing />
+			</div>
 			<footer></footer>
 		</div>
 	);
---
+++ src/components/landing.js
@@ -0,0 +1,13 @@
+
+function Landing() {
+
+    return (
+        <div className="landing">
+            <div className="landing__over-lay">
+                <h1 className="landing__h1">Some Cheesy Slogan01 廣告01放這</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Landing;
