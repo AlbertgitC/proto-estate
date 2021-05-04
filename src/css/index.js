dfd6e659c5078f2d1a85c1a707c00cf9acad9423
Albert Cheng <lptalbert@gmail.com>
2021-05-03 23:26:36 -0700

    update desktop search filter UI

--- src/css/index.js
+++ src/css/index.js
@@ -37,3 +37,4 @@ exports.rental_modal_d = require("./desktop/rental-modal.css");
 exports.rental_form_d = require("./desktop/rental-form.css");
 exports.rental_listing_d = require("./desktop/rental-listing.css");
 exports.photo_gallery_d = require("./desktop/photo-gallery.css");
+exports.search_filter_d = require("./desktop/search-filter.css");
