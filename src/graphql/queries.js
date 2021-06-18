d741babf254bb87911dafaa3da2bfedfed854c1c
Albert Cheng <lptalbert@gmail.com>
2021-06-18 15:36:38 -0700

    add auth to User schema

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,39 +1,6 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const getUser = /* GraphQL */ `
-  query GetUser($id: ID!) {
-    getUser(id: $id) {
-      id
-      email
-      phoneNumber
-      name
-      savedFilter
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const listUsers = /* GraphQL */ `
-  query ListUsers(
-    $filter: ModelUserFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        email
-        phoneNumber
-        name
-        savedFilter
-        createdAt
-        updatedAt
-      }
-      nextToken
-    }
-  }
-`;
 export const getRentalListing = /* GraphQL */ `
   query GetRentalListing($id: ID!) {
     getRentalListing(id: $id) {
@@ -161,6 +128,39 @@ export const rentalListingsSortByCreatedAt = /* GraphQL */ `
         areaPin
         description
         photos
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
+export const getUser = /* GraphQL */ `
+  query GetUser($id: ID!) {
+    getUser(id: $id) {
+      id
+      email
+      phoneNumber
+      name
+      savedFilter
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listUsers = /* GraphQL */ `
+  query ListUsers(
+    $filter: ModelUserFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        email
+        phoneNumber
+        name
+        savedFilter
+        createdAt
         updatedAt
       }
       nextToken
--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,45 +1,6 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const onCreateUser = /* GraphQL */ `
-  subscription OnCreateUser {
-    onCreateUser {
-      id
-      email
-      phoneNumber
-      name
-      savedFilter
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const onUpdateUser = /* GraphQL */ `
-  subscription OnUpdateUser {
-    onUpdateUser {
-      id
-      email
-      phoneNumber
-      name
-      savedFilter
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const onDeleteUser = /* GraphQL */ `
-  subscription OnDeleteUser {
-    onDeleteUser {
-      id
-      email
-      phoneNumber
-      name
-      savedFilter
-      createdAt
-      updatedAt
-    }
-  }
-`;
 export const onCreateRentalListing = /* GraphQL */ `
   subscription OnCreateRentalListing {
     onCreateRentalListing {
@@ -105,6 +66,45 @@ export const onDeleteRentalListing = /* GraphQL */ `
       areaPin
       description
       photos
+      updatedAt
+    }
+  }
+`;
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser {
+    onCreateUser {
+      id
+      email
+      phoneNumber
+      name
+      savedFilter
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser {
+    onUpdateUser {
+      id
+      email
+      phoneNumber
+      name
+      savedFilter
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser {
+    onDeleteUser {
+      id
+      email
+      phoneNumber
+      name
+      savedFilter
+      createdAt
       updatedAt
     }
   }
